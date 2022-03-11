f108f1b03fabe8d1bdcbf7d0957f6f1bb34764b8
gbp12 <gonzabonilla4896@outlook.es>
2022-03-11 10:59:54 +0000

    Updates

--- src/components/Bubble.js
+++
@@ -1,6 +0,0 @@
-import { useSelector } from "react-redux"
-
-export const BubbleAlert = () => {
-	const state = useSelector((x) => x)
-	return <span>{state.contadorProductos > 9 ? "9+" : state.contadorProductos}</span>
-}
--- src/components/VoidList.js
+++
@@ -1,9 +0,0 @@
-import { H3 } from "./Styles"
-
-export const VoidLista = () => {
-	return (
-		<div>
-			<H3>Tu lista está vacía, añade elementos.</H3>
-		</div>
-	)
-}
